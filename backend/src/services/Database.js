067f35e5ff25186e1d42873de2c14e1197faed83
Dammymure <dammymure1@gmail.com>
2024-04-27 16:29:45 +0100

    config

--- backend/src/services/Database.js
+++ backend/src/services/Database.js
@@ -3,7 +3,7 @@ import { MONGO_URI } from "../config/index.js";
 
 const connectToDatabase = async () => {
  try {
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(MONGO_URI, {
    // useNewUrlParser: true,
    // useUnifiedTopology: true,
 
